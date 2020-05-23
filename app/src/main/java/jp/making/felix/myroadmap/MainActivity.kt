9a72a1ff269a0aa6f3c633f7c3ab003a7edc2ec7
felix925 <45941545+felix925@users.noreply.github.com>
2020-05-23 22:56:58 +0900

    feat / app modules

--- app/src/main/java/jp/making/felix/myroadmap/MainActivity.kt
+++ app/src/main/java/jp/making/felix/myroadmap/MainActivity.kt
@@ -2,10 +2,26 @@ package jp.making.felix.myroadmap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.databinding.DataBindingUtil
+import jp.making.felix.myroadmap.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNav.setOnNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.home -> navController.navigate(R.id.fragmentHome)
+            }
+            true
+        }
     }
 }
--- app/src/main/java/jp/making/felix/myroadmap/di/modules.kt
+++ app/src/main/java/jp/making/felix/myroadmap/di/modules.kt
@@ -1,7 +1,8 @@
 package jp.making.felix.myroadmap.di
 
+import jp.making.felix.db.di.dbModule
 import org.koin.core.module.Module
 
 val moduleList:List<Module> = listOf(
-
+    dbModule
 )
